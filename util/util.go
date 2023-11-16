cbe76b1dde7ebad4692dd4d64a598aff9c14bd3e
Xing Yahao <keidarcy.1015@gmail.com>
2023-11-16 09:42:41 +0900

    v1.0.7

--- util/util.go
+++ util/util.go
@@ -23,7 +23,7 @@ const (
 	serviceURLFmt = clusterFmt + serviceFmt + regionFmt
 	taskURLFmt    = clusterFmt + serviceFmt + taskFmt + regionFmt
 
-	AppVersion = "1.0.6"
+	AppVersion = "1.0.7"
 	AppName    = "e1s"
 )
 
