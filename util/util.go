99caa8f7f6f8f02776fb6f485d0dc2a078549180
rromeroatleadtech <43365180+rromeroatleadtech@users.noreply.github.com>
2024-02-01 12:10:15 +0100

    Update util/util.go
    Co-authored-by: Mike Ball <mikedball@gmail.com>

--- util/util.go
+++ util/util.go
@@ -33,7 +33,7 @@ var (
 
 func init() {
 	// init basic logger
-	logPath := fmt.Sprintf("%s/%s-debug.log", os.TempDir(),AppName)
+	logPath := filepath.Join(os.TempDir(), fmt.Sprintf("%s-debug.log", AppName))
 	logFile, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Printf("failed to open log file path: %s, err: %v\n", logPath, err)
