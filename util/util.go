f7763b9f81fb386b445c0f70ad6a335e754404c6
Xing Yahao <keidarcy.1015@gmail.com>
2024-02-07 19:37:14 +0900

    bump: v1.0.18

--- util/util.go
+++ util/util.go
@@ -23,7 +23,7 @@ const (
 	serviceURLFmt = clusterFmt + serviceFmt + regionFmt
 	taskURLFmt    = clusterFmt + serviceFmt + taskFmt + regionFmt
 
-	AppVersion = "1.0.17"
+	AppVersion = "1.0.18"
 	AppName    = "e1s"
 )
 
