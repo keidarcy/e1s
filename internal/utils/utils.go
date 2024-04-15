2d5b142448d6e3b2dc03653fa0efa9dce78f9513
Xing Yahao <keidarcy.1015@gmail.com>
2024-04-16 08:34:58 +0900

    bump: v1.0.27

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -28,7 +28,7 @@ const (
 	serviceURLFmt = clusterFmt + serviceFmt + regionFmt
 	taskURLFmt    = clusterFmt + serviceFmt + taskFmt + regionFmt
 
-	AppVersion = "v1.0.26"
+	AppVersion = "v1.0.27"
 	AppName    = "e1s"
 )
 
