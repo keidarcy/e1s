ec1424e91b950eb097da5193801a21662c609c24
Xing Yahao <keidarcy.1015@gmail.com>
2024-05-04 21:34:41 +0900

    test: add localstack test

--- tests/providers.tf
+++ tests/providers.tf
@@ -8,7 +8,26 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region  = "us-east-1"
   profile = "e1s" // match your ~/.aws/config profile
 }
+
+# provider "aws" {
+#   region     = "us-east-1"
+#   access_key = "mock_access_key" // Use mock credentials for LocalStack
+#   secret_key = "mock_secret_key" // Use mock credentials for LocalStack
+#   profile    = "localstack"      // match your ~/.aws/config profile
+
+#   // LocalStack uses the same endpoint for all services
+#   endpoints {
+#     elasticache = "http://localhost:4566"
+#     ec2         = "http://localhost:4566"
+#     ecs         = "http://localhost:4566"
+#     s3          = "http://localhost:4566"
+#   }
+#   skip_credentials_validation = true
+#   skip_requesting_account_id  = true
+#   skip_metadata_api_check     = true
+# }
