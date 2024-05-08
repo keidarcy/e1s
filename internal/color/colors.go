618c715381940b438c4cc07eea6cb3d0aa3826cb
Xing Yahao <keidarcy.1015@gmail.com>
2024-05-08 09:53:26 +0900

    fix: color theme fallback (#223)

--- internal/color/colors.go
+++ internal/color/colors.go
@@ -12,6 +12,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+var logger *logrus.Logger
+
 type Colors struct {
 	BgColor     string `toml:"background"`
 	FgColor     string `toml:"foreground"`
@@ -26,7 +28,7 @@ type Colors struct {
 	Gray        string `toml:"white"` // Assuming dim white is considered gray
 }
 
-type tomlConfig struct {
+type themeConfig struct {
 	Colors struct {
 		Primary struct {
 			Background string `toml:"background"`
@@ -53,63 +55,22 @@ func Color(c string) tcell.Color {
 }
 
 // Init basic styles
-func InitStyles(theme string, logger *logrus.Logger) Colors {
-	var colors Colors
-	if theme != "" {
-		// Fetch the TOML content from the URL
-		url := fmt.Sprintf("https://raw.githubusercontent.com/keidarcy/alacritty-theme/master/themes/%s.toml", theme)
-		resp, err := http.Get(url)
-		if err != nil {
-			logger.Warnf("Error fetching TOML data: %s", err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			logger.Warnf("Error fetching TOML data: HTTP status code %d", resp.StatusCode)
-		}
-
-		body, err := io.ReadAll(resp.Body)
-		if err != nil {
-			logger.Warnf("Error reading TOML data: %s", err)
-		}
-
-		// Decode the TOML content
-		var config tomlConfig
-		if _, err := toml.Decode(string(body), &config); err != nil {
-			logger.Warnf("Error decoding TOML data: %s", err)
-		}
-
-		// Map the decoded TOML to the Theme struct
-		colors = Colors{
-			BgColor:     config.Colors.Primary.Background,
-			FgColor:     config.Colors.Primary.Foreground,
-			BorderColor: config.Colors.Primary.Foreground, // Assuming border is the same as foreground
-			Black:       config.Colors.Normal.Black,
-			Yellow:      config.Colors.Normal.Yellow,
-			Green:       config.Colors.Normal.Green,
-			Red:         config.Colors.Normal.Red,
-			Blue:        config.Colors.Normal.Blue,
-			Magenta:     config.Colors.Normal.Magenta,
-			Cyan:        config.Colors.Normal.Cyan,
-		}
-	} else {
-		// Default theme gruvbox_dark
-		// Get from https://github.com/alacritty/alacritty-colors/blob/master/themes/gruvbox_dark.toml
-		colors = Colors{
-			BgColor:     "#282828",
-			FgColor:     "#ebdbb2",
-			BorderColor: "#8ec07c",
-			Black:       "#282828",
-			Red:         "#cc241d",
-			Green:       "#98971a",
-			Yellow:      "#d79921",
-			Blue:        "#458588",
-			Magenta:     "#b16286",
-			Cyan:        "#689d6a",
-		}
+func InitStyles(theme string, appLogger *logrus.Logger) Colors {
+	logger = appLogger
+	colors := Colors{
+		BgColor:     "#282828",
+		FgColor:     "#ebdbb2",
+		BorderColor: "#8ec07c",
+		Black:       "#282828",
+		Red:         "#cc241d",
+		Green:       "#98971a",
+		Yellow:      "#d79921",
+		Blue:        "#458588",
+		Magenta:     "#b16286",
+		Cyan:        "#689d6a",
+		Gray:        "#808080",
 	}
-
-	colors.Gray = "#808080"
+	colors.updateByTheme(theme)
 
 	viper.UnmarshalKey("colors", &colors)
 	tview.Styles.PrimitiveBackgroundColor = Color(colors.BgColor)
@@ -127,4 +88,46 @@ func InitStyles(theme string, logger *logrus.Logger) Colors {
 	colors.initFmt()
 
 	return colors
+}
+
+func (c *Colors) updateByTheme(theme string) {
+	if theme == "" {
+		return
+	}
+	// Fetch the TOML content from the URL
+	url := fmt.Sprintf("https://raw.githubusercontent.com/keidarcy/alacritty-theme/master/themes/%s.toml", theme)
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.Warnf("Error fetching TOML data: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Warnf("Error fetching TOML data: HTTP status code %d", resp.StatusCode)
+		return
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Warnf("Error reading TOML data: %s", err)
+		return
+	}
+
+	var config themeConfig
+	// Decode the TOML content
+	if _, err := toml.Decode(string(body), &config); err != nil {
+		logger.Warnf("Error decoding TOML data: %s", err)
+		return
+	}
+	c.BgColor = config.Colors.Primary.Background
+	c.FgColor = config.Colors.Primary.Foreground
+	c.BorderColor = config.Colors.Primary.Foreground
+	c.Black = config.Colors.Normal.Black
+	c.Yellow = config.Colors.Normal.Yellow
+	c.Green = config.Colors.Normal.Green
+	c.Red = config.Colors.Normal.Red
+	c.Blue = config.Colors.Normal.Blue
+	c.Magenta = config.Colors.Normal.Magenta
+	c.Cyan = config.Colors.Normal.Cyan
 }
